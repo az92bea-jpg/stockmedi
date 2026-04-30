7d475e10f9ebf722187c267ff3ced8d22f82f3ff
Alexis Zeze BEAVOGUI <azbea.lomagui@gmail.com>
2026-04-30 13:42:29 +0000

    Correction SMTP Render avec requireTLS et rejectUnauthorized

--- server/src/controllers/contactController.js
+++ server/src/controllers/contactController.js
@@ -6,11 +6,15 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-    port: parseInt(process.env.EMAIL_PORT) || 465,
-    secure: true,
+    port: parseInt(process.env.EMAIL_PORT) || 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
